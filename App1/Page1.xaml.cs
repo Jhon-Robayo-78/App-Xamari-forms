fba48313fb0f9663ca4850182b6b80d1e84210de
Jhon-Robayo-78 <98846026+Jhon-Robayo-78@users.noreply.github.com>
2022-05-08 21:45:44 -0500

    Prueba de subida #3

---
+++ App1/Page1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (TXTusername.Text=="admin" && TXTpassword.Text=="123")
+            {
+                
+                await Navigation.PushAsync(new HomePage());
+            }
+            else {
+               await DisplayAlert("Ops...", "Usuario o contraseña incorrecto", "Ok");
+            }
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Registro());
+        }
+    }
+}
---
+++ App1/Registro.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Ops...", "Error con la conexion a servidor intente mas tarde", "Ok");
+        }
+    }
+}
